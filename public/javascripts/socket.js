07e46c277252f6bc23c884fba8b98d096fe2c2d1
Sergey1759 <lisichka1759@gmail.com>
2020-04-12 07:14:45 +0300

    test deploy

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,5 +1,5 @@
 let form = document.getElementsByTagName('form')[0];
-let socket = io();
+let socket = io('https://cryptic-tundra-22516.herokuapp.com:3000');
 // let room = form.id;
 var objDiv = document.getElementById("messages");
 objDiv.scrollTop = objDiv.scrollHeight;
