9efc65c2eae1b348c1b41f70971bd38c973d742c
Sergey1759 <lisichka1759@gmail.com>
2020-04-12 06:54:37 +0300

    test deploy

--- routes/ApiSocket.js
+++ routes/ApiSocket.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
-http.listen(443);
+http.listen(8080, '34.192.55.25');
 var ApiChats = require("./ApiMessage");
 
 const m = (name, text, id) => {
