cdf8bcfec18ecfa656cd7ca32b4fd887ba6500ac
Sergey1759 <lisichka1759@gmail.com>
2020-04-12 08:16:42 +0300

    test deploy

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,5 +1,8 @@
 let form = document.getElementsByTagName('form')[0];
-let socket = io(`${window.location.hostname}`);
+let socket = io(`${window.location.hostname}`, {
+    port: 8080,
+    transports: ["websocket"]
+});
 // let room = form.id;
 var objDiv = document.getElementById("messages");
 objDiv.scrollTop = objDiv.scrollHeight;
