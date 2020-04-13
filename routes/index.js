e01d79ed85902341c33723482a745aed35ec7044
Sergey1759 <lisichka1759@gmail.com>
2020-04-14 00:51:58 +0300

    add to www socket io for connect to port 3000

--- routes/index.js
+++ routes/index.js
@@ -11,42 +11,6 @@ var ApiMessage = require("./ApiMessage");
 var ApiRooms = require("./ApiRooms");
 var ApiSocket = require("./ApiSocket");
 
-var app = express();
-var server = require('http').Server(app);
-var socketIO = require('socket.io');
-var io = socketIO(server);
-var port = process.env.PORT || 8080;
-server.listen(port, () => {
-  console.log('2')
-})
-var ApiChats = require("./ApiMessage");
-
-const m = (name, text, id) => {
-  return {
-    name: name,
-    text: text,
-    id: id
-  }
-}
-
-io.on('connection', socket => {
-  socket.on('user_join', (data, cb) => {
-    socket.join(data.room)
-    cb({
-      userId: socket.id,
-    });
-    // socket.emit('newMessage', m('admin', `hello ${data.user}`));
-    // socket.broadcast.to(data.room)
-    //     .emit('newMessage', m('admin', `hello2 ${data.user}`))
-  });
-
-  socket.on('CreateMessage', async data => {
-    io.to(data.room).emit('newMessage', m(`${data.user}`, data.text));
-    await ApiChats.createMessage(data.room, data.text, data.user, data.room);
-    console.log(data);
-  });
-
-});
 
 
 
@@ -263,7 +227,7 @@ router.get("/chat/:id", midleware, async function (req, res, next) {
   console.log(req.params);
   let RoomID = req.params.id;
   let massage = await ApiMessage.getChatById(RoomID);
-  console.log(massage);
+  // console.log(massage);
   res.render("chat_id", {
     userID,
     RoomID,
